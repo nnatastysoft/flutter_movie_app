da607f4195b1875cb39618edc04f56d423947fe0
nnatastysoft <nyinyiaung.analyst@gmail.com>
2024-02-08 09:55:03 +0630

    flutter-movie-start

---
+++ lib/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Image.asset(
+          'assets/flutflix.png',
+          fit: BoxFit.cover,
+          height: 40,
+          filterQuality: FilterQuality.high,
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
